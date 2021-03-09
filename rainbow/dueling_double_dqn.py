7ab6418b1ece1e451d7ff9377405c62f9a831164
Tiberio <tiberio-97@hotmail.it>
2021-03-09 19:53:49 +0100

    old models test

--- rainbow/dueling_double_dqn.py
+++ rainbow/dueling_double_dqn.py
@@ -44,9 +44,9 @@ class Agent:
         # Q-Network
         self.qnetwork_value_local = DQN_value(state_size).to(device)
         #self.qnetwork_value_local = GAT_value(state_size, 8, 1, 3, 0.6, 0.2, 4).to(device)
-        #self.qnetwork_value_target = copy.deepcopy(self.qnetwork_value_local)
+        self.qnetwork_value_target = copy.deepcopy(self.qnetwork_value_local)
         self.qnetwork_action = DQN_action(state_size).to(device)
-        self.qnetwork_action = GAT_action(state_size, 8, 2, 3, 0.6, 0.2, 4).to(device)
+        #self.qnetwork_action = GAT_action(state_size, 8, 2, 3, 0.6, 0.2, 4).to(device)
         self.learning_rate = args.learning_rate
         self.optimizer_value = optim.Adam(self.qnetwork_value_local.parameters(), lr=self.learning_rate)
         self.optimizer_action = optim.Adam(self.qnetwork_action.parameters(), lr=self.learning_rate)
